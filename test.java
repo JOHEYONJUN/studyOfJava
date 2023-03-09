44fcf8232e1480ac4f19cc6f856f147c4d79aa5a
NEMURENAIYORU <lhslhs301@gmail.com>
2023-03-09 12:21:13 +0900

    3월9일수업

---
+++ test.java
@@ -0,0 +1,36 @@
+// public class test {
+//     public static void main(String[] args) {
+//         String proverb = "A barking dog"; // new 연산자
+//         String s1, s2, s3, s4; // 참조변수로서 메서드에서 변환된 참조값을 받는다.
+
+//         System.out.println("문자열의 길이 = " + proverb.length());
+
+//         s1 = proverb.concat(" never Bites!"); // 문자열 결합 메서드
+//         s2 = proverb.replace('A', 'a'); // 문자 치환 메서드
+//         s3 = proverb.substring(2, 5); // 문자열 부분추출 메서드
+//         s4 = proverb.toUpperCase(); // 대문자로 변환 메서드
+
+//         System.out.println(s1);
+//         System.out.println(s2);
+//         System.out.println(s3);
+//         System.out.println(s4);
+//     }
+// }
+
+
+
+// public class test {
+//     public static void main(String[] args) {
+//         String hello = "Hello world";
+//         char c = hello.charAt(0);
+//         System.out.println(c); // H
+
+//         String s2 = "Hello world";
+//         System.out.println(hello == s2); // true
+
+//         String s3 = new String("Hello world");
+//         System.out.println(hello == s3); // false s3은 (Original)이니까.
+        
+//     }
+// }
+
