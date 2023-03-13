52998a1acce0f8c419c53c52943495a6738b71b9
NEMURENAIYORU <lhslhs301@gmail.com>
2023-03-13 20:00:14 +0900

    commit test

---
+++ 6jang/test.java
@@ -0,0 +1,16 @@
+// class A {
+//     private int a;
+//     int b;
+//     public int c;
+// }
+
+// public class test {
+//     public static void main(String[] args) {
+//         A obj = new A();
+
+//         obj.a = 10;
+//         obj.b = 20;
+//         obj.c = 30;
+//     }
+// }
+
